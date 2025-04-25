65417ba916d551d3553f6bb5c2903ed0179e35b7
GermanoGallicchio <germano.gallicchio@gmail.com>
2025-04-25 16:00:44 +0100

    Update PCE_Identification_y1x2z3.m
    - cluster forming algorithm a bit more efficient by not re-asking the same points to join a cluster if they said no already (this issue was absent before and was generated by the bulk search)
    
    - written more about the "geometrical" method (rebrand name of "curvature consensus" method). channel gaussian smoothing still in need of writing but not essential for low density montages

--- PCE_Identification_y1x2z3.m
+++ PCE_Identification_y1x2z3.m
@@ -104,7 +104,7 @@ function [clusterMatrix, clustIDList, clusterMetrics] = ...
 %     end
 % end
 
-sigma_x2 = 6;
+sigma_x2 = 3;
 sigma_y1 = 1;
 cLim = [-1 1];
 
@@ -112,7 +112,7 @@ cLim = [-1 1];
 methodList = ["threshold" "geometric"];
 methodIdx = find(strcmp(methodList,PCE_parameters.method));
 switch methodIdx
-    case 1 | 2
+    case {1  2}
         if PCE_parameters.verbose
             disp(['using: ' methodList{methodIdx}])
         end
@@ -132,7 +132,7 @@ end
 [y1Matrix, x2Matrix, z3Matrix] = ndgrid(1:ny1,1:nx2,1:nz3);
 
 
-distThresholdSquared = PCE_parameters.euclDistThreshold^2;
+distThresholdSquared = DimStruct.euclDistThreshold^2;
 
 statMatrix_orig = statMatrix;
 
@@ -232,60 +232,108 @@ switch methodIdx
     case 1  % EFthreshold
 
     case 2  % curvatureConsensus
+        %% 1d -> 3d
+
+        % reshape from 1d to 3d space
+        statMatrix_3d = reshape(statMatrix,[ny1 nx2 nz3]);
+
         %% gaussian smoothing
         % of 2d continuous dimensions
-
+        
         % gaussian along x2
-        domain_x2 = -ceil(3*sigma_x2):ceil(3*sigma_x2);
+        if nx2 > 1
+            domain_x2 = -ceil(3*sigma_x2):ceil(3*sigma_x2);
+        else
+            domain_x2 = 1;
+        end
         g_x2 = exp(-domain_x2.^2/(2*sigma_x2^2));
         g_x2 = g_x2/sum(g_x2); % normalize gaussian to [0, 1]
 
         % gaussian along y1
-        domain_y1 = -ceil(3*sigma_y1):ceil(3*sigma_y1);
+        if ny1 > 1
+            domain_y1 = -ceil(3*sigma_y1):ceil(3*sigma_y1);
+        else
+            domain_y1 = 1;
+        end
         g_y1 = exp(-domain_y1.^2/(2*sigma_y1^2));
         g_y1 = g_y1/sum(g_y1); % normalize gaussian to [0, 1]
 
-        statMatrix = zeros(size(statMatrix));
+        % gaussian smoothing one dimension at the time
         for z3Idx = 1:nz3
-            temp = conv2(squeeze(statMatrix_orig(z3Idx,:,:)), g_x2, 'same');
+            temp = conv2(squeeze(statMatrix_3d(:,:,z3Idx)), g_x2, 'same');
             result = conv2(temp, g_y1', 'same');
-            statMatrix(z3Idx,:,:) = result;
+            statMatrix_3d(:,:,z3Idx) = result;
         end
 
         % of channel dimension
-        %...
+        % it might not be needed for a low density EEG (eg, 32 channels)
+        %... TO DO: find a way to do smoothing in the channel dimension
 
         %% second partial derivatives
 
-        partialDerivatives = zeros(1,3); % logic to tell which partial derivatives are computed
+        partialDerivatives = false(1,3); % logic to tell which partial derivatives are computed
 
         % y1 dimension
         if ny1<=3
-            warning('the second derivative is not meaningful with fewer than 3 points. so i will not apply the second derivative on this dimension')
+            if PCE_parameters.verbose
+                warning(['skipping y1 dimension (n = ' num2str(ny1) '). the second derivative is not meaningful with fewer than 3 points. so i will not apply the second derivative on this dimension'])
+            end
         else
+            partialDerivatives(1) = true;
             d2y1 = zeros(size(statMatrix));
-            d2y1(:,2:end-1,:) = statMatrix(:,3:end,:) -2*statMatrix(:,2:end-1,:) +statMatrix(:,1:end-2,:);
-            d2y1(:,1,:)       = statMatrix(:,3,    :) -2*statMatrix(:,2,      :) +statMatrix(:,1,      :); % first edge (1), same as value at index 2
-            d2y1(:,end,:)     = statMatrix(:,end,  :) -2*statMatrix(:,end-1,  :) +statMatrix(:,end-2,  :); % last edge (end), same as value at index end-1
-            partialDerivatives(2) = 1;
+            d2y1(2:end-1,:,:) = statMatrix(3:end,:,:) -2*statMatrix(2:end-1,:,:) +statMatrix(1:end-2,:,:);
+            d2y1(1,:,:)       = statMatrix(3,    :,:) -2*statMatrix(2,      :,:) +statMatrix(1,      :,:); % first edge (1), same as value at index 2
+            d2y1(end,:,:)     = statMatrix(end,  :,:) -2*statMatrix(end-1,  :,:) +statMatrix(end-2,  :,:); % last edge (end), same as value at index end-1
         end
 
         % x2 dimension
         if nx2<=3
-            warning('the second derivative is not meaningful with fewer than 3 points. so i will not apply the second derivative on this dimension')
+            if PCE_parameters.verbose
+                warning(['skipping x2 dimension (n = ' num2str(nx2) '). the second derivative is not meaningful with fewer than 3 points. so i will not apply the second derivative on this dimension'])
+            end
         else
-            d2x2 = zeros(size(statMatrix));
-            d2x2(:,:,2:end-1) = statMatrix(:,:,3:end) -2*statMatrix(:,:,2:end-1) +statMatrix(:,:,1:end-2);
-            d2x2(:,:,1)       = statMatrix(:,:,3    ) -2*statMatrix(:,:,2      ) +statMatrix(:,:,1      ); % first edge (1), same as value at index 2
-            d2x2(:,:,end)     = statMatrix(:,:,end  ) -2*statMatrix(:,:,end-1  ) +statMatrix(:,:,end-2  ); % last edge (end), same as value at index end-1
-            partialDerivatives(3) = 1;
+            partialDerivatives(2) = true;
+            d2x2 = zeros(size(statMatrix_3d));
+            % consider the triplet [a b c] and h1 = distance a to b and h2 = distance c to b
+            a = statMatrix_3d(:,1:end-2,:);
+            b = statMatrix_3d(:,2:end-1,:);
+            c = statMatrix_3d(:,3:end,:);
+            h = repmat(diff(DimStruct.x2_vec),ny1,1,nz3);
+            h1 = abs(h(:,1:end-1,:));
+            h2 = abs(h(:,2:end,:));
+            if ~isequal(size(a), size(b), size(c), size(h1), size(h2))
+                error('error in the continuous second partial derivative')
+            end
+            d2x2(:,2:end-1,:) = (h2.*c -(h1+h2).*b +h1.*a) ./ (h1.*h2.*(h1+h2));
+            d2x2(:,1,      :) = (h2(:,1,:).*c(:,1,:) -(h1(:,1,:)+h2(:,1,:)).*b(:,1,:) +h1(:,1,:).*a(:,1,:)) ./ (h1(:,1,:).*h2(:,1,:).*(h1(:,1,:)+h2(:,1,:)));
+            % isequal(d2x2(:,1,      :), d2x2(:,2,      :)) % first and second must be equal, because we imagine a continuation of the curvature to the edge points
+            d2x2(:,end,      :) = (h2(:,end,:).*c(:,end,:) -(h1(:,end,:)+h2(:,end,:)).*b(:,end,:) +h1(:,end,:).*a(:,end,:)) ./ (h1(:,end,:).*h2(:,end,:).*(h1(:,end,:)+h2(:,end,:)));
+            % isequal(d2x2(:,end,      :), d2x2(:,end-1,      :)) % last and second last must be equal, because we imagine a continuation of the curvature to the edge points
         end
 
         % z3 dimension
-        if nz3>1
-            partialDerivatives(1) = 1;
-            error('this is not coded yet. it will when i use multi channel data') % temporary error
-            % use hyorth laplacian method?
+        if nz3<=20
+            if PCE_parameters.verbose
+                warning(['skipping y1 dimension (n = ' num2str(ny1) '). the second derivative is not meaningful with fewer than 3 points. so i will not apply the second derivative on this dimension'])
+            end
+        else
+            partialDerivatives(3) = true;
+            d2z3 = zeros(size(statMatrix_3d));
+            for z3Idx = 1:nz3
+                % neighbors: idx, num, and values (in 3d)
+                neighIdx = find(DimStruct.z3_Dbinned(z3Idx,:)==1);
+                neighNum = length(neighIdx);
+                neigh = statMatrix_3d(:,:,neighIdx);
+                % neighbors: idx and values (in 3d)
+                seedIdx = z3Idx;
+                seed = repmat(statMatrix_3d(:,:,seedIdx),1,1,1); % the 3rd dimension does not need to be repeated per neighNum because neighNum in the other variables will be summed across
+                % angle between each neighbor and the seed: values (in 3d)
+                theta = repmat(reshape(DimStruct.z3_D(neighIdx,seedIdx),1,1,neighNum),ny1,nx2,1);
+                if ~isequal(size(neigh),size(theta),[size(seed) neighNum])
+                    error('error in the channel second partial derivative')
+                end
+                d2z3(:,:,z3Idx) = sum(neigh./(theta.^2),3) - seed.*sum(1/(theta.^2),3);
+            end
         end
 
         % sign consensus among the curvature of each dimension
@@ -296,7 +344,7 @@ switch methodIdx
                     curvatureConsensus = ((sign(d2y1)==sign(d2x2)) & (sign(d2y1)==sign(d2z3))).*sign(d2y1);
                 else
                     % active dimensions [1 1 0]
-                    error('case not encountered yet. it will be coded at a later time')
+                    curvatureConsensus = (sign(d2y1)==sign(d2x2)).*sign(d2y1);
                 end
             else
                 if partialDerivatives(3)
@@ -311,7 +359,7 @@ switch methodIdx
             if partialDerivatives(2)
                 if partialDerivatives(3)
                     % active dimensions [0 1 1]
-                    curvatureConsensus = (sign(d2y1)==sign(d2x2)).*sign(d2y1);
+                    curvatureConsensus = (sign(d2x2)==sign(d2z3)).*sign(d2x2);
                 else
                     % active dimensions [0 1 0]
                     error('case not encountered yet. it will be coded at a later time')
@@ -326,18 +374,36 @@ switch methodIdx
                 end
             end
         end
-end
 
-adjMatrix = PCE_parameters.adjMatrix;
+        %% combine sign of curvature and of values
+        curvature = zeros(size(curvatureConsensus));
+
+        % positive curvature & negative value = a negative bump (a valley)
+        curvature(curvatureConsensus>0 & statMatrix_3d<0) = -1;
+        
+        % negative curvature & positive value = a positive bump (a hill)
+        curvature(curvatureConsensus<0 & statMatrix_3d>0) = 1;
+
+        clear curvatureConsensus % free space
+
+        %% 3d -> 1d
+
+        % reshape from 1d to 3d space
+        curvature = reshape(curvature,[1 ny1*nx2*nz3]);
+
+        clear statMatrix_3d % free space
+end
 
 %% cluster forming
 % find cluster IDs
 
+adjMatrix = PCE_parameters.adjMatrix;
+
 switch methodIdx
     case 1 % threshold
-        searchMatrix = (abs(statMatrix)>PCE_parameters.threshold .*  PCE_parameters.ignoreMask) .* sign(statMatrix);
+        searchMatrix = (abs(statMatrix)>PCE_parameters.threshold) .* sign(statMatrix);
     case 2 % geometric
-        searchMatrix = curvatureConsensus;
+        searchMatrix = abs(statMatrix) .* curvature ;
 end
 
 clusterMatrix = zeros(size(statMatrix)); % initialize. will show cluster membership
@@ -351,7 +417,6 @@ searchMatrix = searchMatrix .* PCE_parameters.ignoreMask;
 for pnt_idx = find(searchMatrix)    % loop through the searchMatrix points
 
     % if this cluster is not assigned, assign it to the next available cluster
-    %if recruitedMatrix(pnt_idx)==0
     if clusterMatrix(pnt_idx)==0
 
         pnt_sign = sign(searchMatrix(pnt_idx));
@@ -363,6 +428,7 @@ for pnt_idx = find(searchMatrix)    % loop through the searchMatrix points
         end
         clusterMatrix(pnt_idx) = clustID;
         firstRecruiter = true;
+        askedMatrix = false(size(statMatrix)); % initialize askedMatrix to keep track of which points have been asked to join this specific cluster
     else
         continue % move on to the next point
     end
@@ -373,28 +439,36 @@ for pnt_idx = find(searchMatrix)    % loop through the searchMatrix points
         
         if firstRecruiter
             queueMatrix(pnt_idx) = true;
+            askedMatrix(pnt_idx) = true;
         end
 
         % find neighbors. points need to be...
         % ...in the adjacency matrix
-        % criteria = adjMatrix(recruiter_idx,:); older
         criteria = adjMatrix(queueMatrix,:);  % some of these points are counted more than once (same of these points share neighbors)
         criteria = sum(criteria,1)>0;  % this line removed doubles
-        if size(criteria,1)~=1
-            error('criteria should be a row vecor')
-        end
+%         if size(criteria,1)~=1
+%             error('criteria should be a row vecor')
+%         end
 
         % ...in the searchMatrix and with the same sign as pnt_idx
-        criteria = criteria & searchMatrix==pnt_sign;
+        criteria = criteria & sign(searchMatrix)==pnt_sign;
 
         % ...unassigned to a cluster
         criteria = criteria & clusterMatrix==0;
 
-        % ...not already in the queue (maybe?) NO: DELETE THIS CRITERION
-        %             criteria = criteria & queueMatrix==0;
+        % ...not already asked to join this cluster 
+        % because of the bulk search, some points are asked multiple times to joint the same cluster
+        % TO DO :
+        % - create a askedMatrix logic similar to queue, which is reset at the beginning of a new cluster 
+        % - do something similar to this: 
+        criteria = criteria & ~askedMatrix;
+
 
         % assign these points to this cluster
         clusterMatrix(criteria) = clustID;
+
+        % assign this point to the askMatrix
+        askedMatrix(criteria) = true;
 
         % remove the current point(s) from the queue (their job is done)
         %queueMatrix(recruiter_idx) = false; % older
@@ -516,7 +590,7 @@ if PCE_parameters.figFlag_cluster==1
         z3Idx = randi(nz3,1)
 
         nexttile(lyt)
-        mat2plot = squeeze(statMatrix_orig(z3Idx,:,:));
+        mat2plot = squeeze(statMatrix_orig(:,:,z3Idx));
         contour2plot_neg = logical(squeeze(clusterMatrix(z3Idx,:,:)<0));
         contour2plot_pos = logical(squeeze(clusterMatrix(z3Idx,:,:)>0));
         im = imagesc(DimStruct.x2_vec,DimStruct.y1_vec,mat2plot);
@@ -557,17 +631,22 @@ if PCE_parameters.figFlag_cluster==1
             horizAxisLim = DimStruct.x2_vec([1 end]);
             
         end
-        line2plot = squeeze(statMatrix(:,:,z3Idx));
-
-        line2plot_negIdx = logical(squeeze(clusterMatrix(:,:,z3Idx)<0));
-        line2plot_posIdx = logical(squeeze(clusterMatrix(:,:,z3Idx)>0));
+        % line to plot
+        tmp = reshape(statMatrix_orig,[ny1 nx2 nz3]);
+        line2plot = tmp(:,:,z3Idx);
         pl = plot(horizAxisVals,line2plot,'.-','Color',[0 0 0]);
         pl.Parent.XAxis.Label.String = horizAxisLbl;
         pl.Parent.XLim = horizAxisLim;
         hold on
-        line(repmat(pl.Parent.XLim',1,2), PCE_parameters.EFthreshold*[-1 1; -1 1],'LineStyle','--')
-        plot(horizAxisVals(line2plot_negIdx),line2plot(line2plot_negIdx),'.','Color',[0 0 1]);
-        plot(horizAxisVals(line2plot_posIdx),line2plot(line2plot_posIdx),'.','Color',[1 0 0]);
+
+        tmp = reshape(clusterMatrix,[ny1 nx2 nz3]);
+        line2plot_negIdx = tmp(:,:,z3Idx) < 0;
+        line2plot_posIdx = tmp(:,:,z3Idx) > 0;
+        line(repmat(pl.Parent.XLim',1,2), PCE_parameters.threshold*[-1 1; -1 1],'LineStyle','--')
+        line(pl.Parent.XLim, zeros(1,2),'LineStyle','--','Color',[0 0 0 0.2])
+
+        plot(horizAxisVals(line2plot_negIdx),line2plot(line2plot_negIdx),'o','Color',[0 0 1]);
+        plot(horizAxisVals(line2plot_posIdx),line2plot(line2plot_posIdx),'o','Color',[1 0 0]);
         hold on
         title([ DimStruct.z3_lbl ': ' DimStruct.z3_chanlocs(z3Idx).labels])
 
@@ -639,7 +718,7 @@ for clIdx = 1:nClust
 %     end
 
     clusterMetrics_size(1,clIdx) = sum(idx(:));
-    clusterMetrics_mass(1,clIdx) = sum(statMatrix(idx)); % can be positive or negative depending on content (ie, interest in pos or neg clusters)
+    clusterMetrics_mass(1,clIdx) = sum(statMatrix_orig(idx)); % can be positive or negative depending on content (ie, interest in pos or neg clusters)
 
 end
 
