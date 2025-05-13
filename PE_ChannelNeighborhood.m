f6a8cc70d4889518161747dcfa6ded15258ce15d
GermanoGallicchio <germano.gallicchio@gmail.com>
2025-05-14 00:06:47 +0100

    Update PE_ChannelNeighborhood.m
    added documentation and more sanity checks

--- PE_ChannelNeighborhood.m
+++ PE_ChannelNeighborhood.m
@@ -1,47 +1,165 @@
 function [ neighborMatrix, D, Dbinned ] = ...
-    ClusterAnalysis_ChannelNeighborhood(chanlocs, dist_threshold, angDistFig_flag, topoPlotFig_flag, topoPlotFig_chanLbl, eeglabPath)
+    PE_ChannelNeighborhood(chanlocs, dist_threshold, angDistFig_flag, topoPlotFig_flag, topoPlotFig_chanLbl, eeglabPath)
+% Identifies neighboring EEG channels based on angular distance
 
-% This function identifies clusters along certain dimensions
-%
-% inspired by Groppe's spatial_neighbors.m
-% https://github.com/dmgroppe/Mass_Univariate_ERP_Toolbox but using angular
-% instead of linear distance. This function, like Groppe's, assumes
-% electrodes are placed on a sphere (i.e., sensor sites have same radius)
+%   [neighborMatrix, D, Dbinned] = PE_ChannelNeighborhood(chanlocs, dist_threshold, 
+%   angDistFig_flag, topoPlotFig_flag, topoPlotFig_chanLbl, eeglabPath) 
+%   determines neighboring channels based on their angular distances on a spherical head model
 %
 %
+%   INPUT:
 %
-% INPUT:
-%
-% chanlocs              channel location structure just like eeglab's EEG.chanlocs structure
-%
-% dist_threshold        angular distance (in radians) below which channels will be considered neighbord
-%                       / 0.36 works well for 128 channels (biosemi)
-%                       / 0.7 works well for 32 channels (biosemi)
-%
-% angDistFig_flag       Plot angular distance matrix and histogram (1=yes)
-%
-% topoPlotFig_flag      Plot topographic map of seeds and neighbors (1=yes)
-%
-% topoPlotFig_chanLbl   seed channel(s) for the topoplots. The topoPlot will show the neighbors of each seed
-%
-% eeglabPath            String with path to eeglab to be fed to addpath(). Needed for the topoPlotFig
+%       chanlocs         - EEGLAB channel location structure (EEG.chanlocs)
+%                         Must contain fields: sph_theta, sph_phi, labels
+%       dist_threshold   - Angular distance threshold in radians below which
+%                         channels are considered neighbors
+%                         Recommended values:
+%                         - 0.36 for 128-channel systems (Biosemi)
+%                         - 0.70 for 32-channel systems (Biosemi)
+%       angDistFig_flag  - Flag to plot distance matrix and histogram (1=yes, 0=no)
+%       topoPlotFig_flag - Flag to plot topographic maps (1=yes, 0=no)
+%       topoPlotFig_chanLbl - Cell array of channel labels for topoplots. seed channel(s) for the topoplots. 
+%       eeglabPath      - String containing path to EEGLAB (required for topoplots)
 %
 %
-% OUTPUT:
 %
-% neighborMatrix        NxN matrix (N = num of channels) of 0s and 1s
-%                       1=the two channels are neighbors, 0=not neighbors
+%   OUTPUT:
 %
-% D                     NxN matrix with angular distances
+%       neighborMatrix   - N×N binary matrix (N = number of channels)
+%                         1 = channels are neighbors, 0 = not neighbors
+%       D               - N×N matrix of angular distances in radians
+%       Dbinned        - N×N matrix of binned distances where:
+%                         0 = same channel
+%                         1 = within dist_threshold (neighbors)
+%                         2 = within 2*dist_threshold
+%                         etc.
 %
-% Dbinned               NxN matrix with binned angular distances (0=same channel, 1=neighbor, 2=within twice as far, etc)
+%   This function is inspired by Groppe's spatial_neighbors.m
+%   (https://github.com/dmgroppe/Mass_Univariate_ERP_Toolbox)
+%   but uses angular instead of linear distance. 
+%   This function, like Groppe's, assumes electrodes are placed 
+%   on a sphere (i.e., sensor sites have same radius)
 %
-% written by Germano Gallicchio 
-% germano.gallicchio@gmail.com
+%   Author: Germano Gallicchio (germano.gallicchio@gmail.com)
 
-%% debugging cell
+%% sanity checks
+
+% Check number of input arguments
+narginchk(2, 6);
+
+% default values for optional inputs
+if nargin < 3
+    angDistFig_flag = 0;
+end
+if nargin < 4
+    topoPlotFig_flag = 0;
+end
+if nargin < 5
+    topoPlotFig_chanLbl = {};
+end
+if nargin < 6
+    eeglabPath = '';
+end
 
 
+% check DimStruct is a structure
+if ~isstruct(DimStruct)
+    error('PE_Adjacency_y1x2z3:InvalidInput', ...
+        'DimStruct must be a structure.');
+end
+
+% required fields in DimStruct
+required_fields = {'y1_lbl', 'y1_contFlag', 'y1_vec', 'y1_units', ...
+                  'x2_lbl', 'x2_contFlag', 'x2_vec', 'x2_units', ...
+                  'z3_lbl', 'z3_contFlag', 'z3_chanlocs', 'z3_neighborMatrix', ...
+                  'euclDistThreshold', 'angDistThreshold', 'z3_D'};
+
+% check if there are missing fields
+missing_fields = setdiff(required_fields, fieldnames(DimStruct));
+if ~isempty(missing_fields)
+    error('PE_Adjacency_y1x2z3:MissingFields', ...
+          'The following fields are missing in DimStruct: %s', ...
+          strjoin(missing_fields, ', '));
+end
+
+% check data types and dimensions
+if ~ischar(DimStruct.y1_lbl) || ~ischar(DimStruct.x2_lbl) || ~ischar(DimStruct.z3_lbl)
+    error('PE_Adjacency_y1x2z3:InvalidLabels', 'Labels must be character arrays.');
+end
+
+if ~isnumeric(DimStruct.y1_vec) || ~isvector(DimStruct.y1_vec) || ...
+   ~isnumeric(DimStruct.x2_vec) || ~isvector(DimStruct.x2_vec)
+    error('PE_Adjacency_y1x2z3:InvalidVectors', ...
+        'y1_vec and x2_vec must be numeric vectors.');
+end
+
+if ~isnumeric(DimStruct.euclDistThreshold) || ~isscalar(DimStruct.euclDistThreshold) || ...
+   DimStruct.euclDistThreshold <= 0
+    error('PE_Adjacency_y1x2z3:InvalidThreshold', ...
+          'euclDistThreshold must be a positive numeric scalar.');
+end
+
+if ~isnumeric(DimStruct.angDistThreshold) || ~isscalar(DimStruct.angDistThreshold) || ...
+   DimStruct.angDistThreshold <= 0
+    error('PE_Adjacency_y1x2z3:InvalidThreshold', ...
+          'angDistThreshold must be a positive numeric scalar.');
+end
+
+if ~isnumeric(DimStruct.z3_D) || ~ismatrix(DimStruct.z3_D)
+    error('PE_Adjacency_y1x2z3:InvalidDistanceMatrix', ...
+          'z3_D must be a numeric matrix.');
+end
+
+% check continuous flags
+if ~isscalar(DimStruct.y1_contFlag) || ~ismember(DimStruct.y1_contFlag, [0,1]) || ...
+   ~isscalar(DimStruct.x2_contFlag) || ~ismember(DimStruct.x2_contFlag, [0,1]) || ...
+   ~isscalar(DimStruct.z3_contFlag) || ~ismember(DimStruct.z3_contFlag, [0,1])
+    error('PE_Adjacency_y1x2z3:InvalidcontFlags', ...
+          'contFlag fields must be scalar values of 0 or 1.');
+end
+
+% check adjFig is logical
+if ~islogical(adjFig) && ~(isnumeric(adjFig) && ismember(adjFig, [0,1]))
+    error('PE_Adjacency_y1x2z3:InvalidAdjFig', ...
+          'adjFig must be a logical value or 0/1.');
+end
+
+
+
+
+% check topoPlotFig_chanLbl if topoplot is requested
+if topoPlotFig_flag
+    if ~iscell(topoPlotFig_chanLbl) && ~isstring(topoPlotFig_chanLbl)
+        error('PE_ChannelNeighborhood:InvalidChannelLabels', ...
+              'topoPlotFig_chanLbl must be a cell array or string array of channel labels.');
+    end
+    
+    % convert to cell array if string array
+    if isstring(topoPlotFig_chanLbl)
+        topoPlotFig_chanLbl = cellstr(topoPlotFig_chanLbl);
+    end
+    
+    % check channel labels exist in chanlocs
+    channel_labels = {chanlocs.labels};
+    invalid_channels = setdiff(topoPlotFig_chanLbl, channel_labels);
+    if ~isempty(invalid_channels)
+        error('PE_ChannelNeighborhood:InvalidChannelLabels', ...
+              'The following channel labels were not found in chanlocs: %s', ...
+              strjoin(invalid_channels, ', '));
+    end
+end
+
+% check eeglabPath when topoplot is requested
+if topoPlotFig_flag && (~ischar(eeglabPath) && ~isstring(eeglabPath))
+    error('PE_ChannelNeighborhood:InvalidEEGLABPath', ...
+          'eeglabPath must be a character vector or string.');
+end
+
+% check eeglabPath exists when provided
+if ~isempty(eeglabPath) && ~exist(eeglabPath, 'dir')
+    error('PE_ChannelNeighborhood:InvalidEEGLABPath', ...
+          'The specified EEGLAB path does not exist: %s', eeglabPath);
+end
 
 %% get info
 
