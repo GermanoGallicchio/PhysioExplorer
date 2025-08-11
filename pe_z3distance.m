df577ad77ac15faf41cdb80abde2d1838eaee2dd
GermanoGallicchio <germano.gallicchio@gmail.com>
2025-08-11 21:45:35 +0100

    Create pe_z3distance.m

---
+++ pe_z3distance.m
@@ -0,0 +1,248 @@
+function z3_distanceMatrix = pe_z3distance(pe_cfg, eeglabPath)
+
+% Computes angular pairwise distances between sensors. The distance matrix
+% is later (pe_adjacency) used to identify which channels are in each channel's
+% "neighborhood".
+% 
+% Optionally, draws a multiplot figure
+%   - distance matrix 
+%   - histogram
+%   - topographic map showing channel neighborhood for user-selected channel(s)
+%   - that graph I made for the Alcohol study
+%
+%   INPUT:
+%
+%       pe_cfg
+%
+%   OUTPUT:
+%
+%       z3_distanceMatrix          - N×N matrix of angular distances in radians (N = num of channels)
+%
+% Acknowledgments:
+%   This function is inspired on Groppe's spatial_neighbors.m
+%   (https://github.com/dmgroppe/Mass_Univariate_ERP_Toolbox)
+%   but uses angular instead of linear distance. 
+%   This function, like Groppe's, assumes electrodes are placed 
+%   on a sphere (i.e., sensor sites have same radius)
+%
+%   Author: Germano Gallicchio (germano.gallicchio@gmail.com)
+
+
+% TO DO: in future, remove dependency from eeglab's topoplot by creating my own topoplot function
+
+%% shortcuts
+
+chanlocs = pe_cfg.dimensions.z3_chanLocs;
+nChans = pe_cfg.dimensions.z3_num;
+
+% angular coordinates (in radians)
+theta = deg2rad([chanlocs(:).sph_theta]);
+phi   = deg2rad([chanlocs(:).sph_phi]);
+
+distance_z3_angular = pe_cfg.clusterParams.distance_z3_angular;
+
+%% angular distance matrix
+
+% compute angular distance between all pairs
+z3_distanceMatrix = zeros(nChans, nChans); % initialize D
+for chanAIdx = 1:nChans
+    for chanBIdx = 1:nChans
+        if chanAIdx==chanBIdx
+            angDist = 0;
+        else
+            angDist = acos( sin(phi(chanAIdx))*sin(phi(chanBIdx))  + cos(phi(chanAIdx))*cos(phi(chanBIdx))*cos(theta(chanAIdx)-theta(chanBIdx)) );
+        end
+        z3_distanceMatrix(chanAIdx,chanBIdx) = angDist;
+    end
+end
+
+
+%% discretized angular distance matrix
+
+% discretize sensor distance and define neighbors
+%   0 = same channel 
+%   1 = within dist_threshold
+%   2 = within dist_threshold*2
+%   3 = within dist_threshold*3
+%   ...
+%   n = within Max(D)
+% create edges from 0 to max(D) in steps of dist_threshold, making sure the upper edge is included too
+angularEdges = 0:distance_z3_angular:max(z3_distanceMatrix(:));
+if angularEdges(end)~=max(z3_distanceMatrix(:));     angularEdges(end+1) = max(z3_distanceMatrix(:));  end
+Dbinned = -999*ones(size(z3_distanceMatrix)); % initialize
+dIdx = z3_distanceMatrix==angularEdges(1);
+Dbinned(dIdx) = angularEdges(1);
+for eIdx = 1:length(angularEdges)-1
+    dIdx = z3_distanceMatrix>angularEdges(eIdx)  &  z3_distanceMatrix<=angularEdges(eIdx+1);
+    Dbinned(dIdx) = eIdx;
+end
+%   neighbor = if discretized distance == 1 (a sensor cannot be neighbor with itself)
+neighborMatrix = Dbinned==1;
+neighborNum = sum(neighborMatrix,1);  % number of neighbors for each channel
+
+
+
+%% figure
+
+if pe_cfg.figFlag==1
+
+    %% figure setup
+    figure(); clf
+    f = gcf;
+    f.Units = 'normalized'; f.Position = [0.1 0.01 0.8 0.9];
+
+    panelA = uipanel('Position',[0  0.4  1  0.6]);
+    panelB = uipanel('Position',[0  0    0.5  0.4]);
+    panelC = uipanel('Position',[0.5  0    0.5  0.4]);
+
+    %% figure panel 2:
+    % topoplot
+
+    % plot channel neighbors
+    if pe_cfg.figFlag
+
+        % check EEGLAB path
+        if ~exist('topoplot', 'file')
+            if isempty(eeglabPath)
+                error('EEGLAB''s topoplot function not found. Valid eeglabPath must be provided.');
+            end
+            addpath(eeglabPath);
+            if ~exist('topoplot', 'file')
+                error('PE_ChannelNeighborhood:EEGLABNotFound', ...
+                    'topoplot function not found in the provided EEGLAB path.');
+            end
+        end
+
+
+        % choose random channel idx
+        topoPlotFig_chanIdx = sort(randperm(nChans,8));
+        topoPlotFig_chanLbl = string({chanlocs(topoPlotFig_chanIdx).labels});
+        % propose the random selection to the user
+        list = string({chanlocs(:).labels});
+        [idx,tf] = listdlg('ListString',list,'SelectionMode','multiple','InitialValue',topoPlotFig_chanIdx,'ListSize',[300 200],'PromptString',{'pure visualization purposes:' 'view the neighborhood of selected seed channels' 'display optimized for 8 channels' '(i preselected a few randomly)'});
+        if tf==0
+            idx=topoPlotFig_chanIdx; warning(['You did not select, so i kept the random selection'])
+        else
+            % update the selection of channels
+            topoPlotFig_chanIdx = idx;
+            topoPlotFig_chanLbl = string({chanlocs(idx).labels});
+        end
+        chanIdx = topoPlotFig_chanIdx;
+        chanLbl = topoPlotFig_chanLbl;
+
+        % pick tile layout options
+        if length(chanLbl)==8
+            nrow = 2;
+            ncol = 4;
+            tldB = tiledlayout(panelB,nrow,ncol);
+        else
+            tldB = tiledlayout(panelB,'flow');
+        end
+        tldB.Padding = 'tight';
+        tldB.TileSpacing = 'tight';
+
+        % topoplot-specific parameters
+        radius = 0.70; % for plotting reasons only (radius is not in the computations)
+        xyLim = [-0.455 0.455];  % to make the circles appear bigger
+
+        plotCounter = 0;
+        for cIdx = 1:length(chanLbl)
+            plotCounter = plotCounter + 1;
+
+            nexttile(tldB)
+            surrogateData = randn(1,nChans);
+            % all chans
+            topoplot(surrogateData, chanlocs, 'electrodes', 'on', 'style', 'blank', 'plotrad',radius, 'headrad', 0.5, 'emarker', {'o', 0.6*[1 1 1],3,1}); hold on;  % plot all channels
+            set(gca,'XLim',xyLim, 'YLim',xyLim)
+            % seed chan
+            chan2plot = chanIdx(cIdx);
+            topoplot(surrogateData(chan2plot ), chanlocs(chan2plot),  'electrodes', 'on', 'style', 'blank', 'plotrad',radius, 'headrad', 0, 'emarker', {'.',[0    0.4470    0.7410], 15,5}); hold on; % plot seed channel
+            set(gca,'XLim',xyLim, 'YLim',xyLim)
+            % neighbor chans
+            chan2plot = neighborMatrix(chanIdx(cIdx),:);
+            if sum(chan2plot)>=1
+                topoplot(surrogateData(chan2plot ), chanlocs(chan2plot),  'electrodes', 'on', 'style', 'blank', 'plotrad',radius, 'headrad', 0, 'emarker', {'.',[0.8500    0.3250    0.0980], 15,5}); hold on; % plot neighbors of seed channels
+                set(gca,'XLim',xyLim, 'YLim',xyLim)
+            end
+            %ttl = title({['seed channel: ' chanLbl{cIdx}] [num2str(sum(chan2plot)) ' neighbors' ]});
+            ttl = title(['seed: ' chanLbl{cIdx}]);
+            ttl.FontWeight = 'normal';
+            ttl.VerticalAlignment = 'bottom';
+            ttl.FontSize = 10;
+            %ttl.Position(2) = 0.55;
+
+        end
+        sgtitle('neighbors (in red) of seed channels (in blue)')
+
+    end
+
+    %% figure panel 1: 
+    % angular distance matrix
+
+    tldA = tiledlayout(panelA,'flow');
+    
+    nexttile(tldA)
+    mt = imagesc(1:nChans, 1:nChans, z3_distanceMatrix);
+    mt.Parent.YTick = 1:nChans;
+    mt.Parent.YTickLabel = string({chanlocs.labels});
+    mt.Parent.YLabel.String = 'channel labels';
+    mt.Parent.XTick = 1:nChans;
+    mt.Parent.XTickLabel = string({chanlocs.labels});
+    mt.Parent.XLabel.String = 'channel labels';
+    mt.Parent.Colormap = flipud(copper);
+    cb = colorbar;
+    cb.Label.String = 'angular distance [rad]';
+
+
+    
+    nexttile(tldA)
+    counts = tril(z3_distanceMatrix); counts(counts==0)=NaN; % keep only each inter-channel distance
+    hst = histogram(counts(:), 'BinWidth',0.1);
+    hst.Parent.YLabel.String = {'num of channel pairs' 'at a certain distance'};
+    hst.Parent.XLabel.String = 'angular distance [rad]';
+    xline(distance_z3_angular,'Color',[1 0 0],'LineStyle','--','LineWidth',1)
+
+    %% figure panel 3:
+    % which threshold yields the most similar number of neighbors across channels
+
+
+    tldC = tiledlayout(panelC,'flow');
+    dVal = logspace(log10(0.5+1),log10(3+1),400)-1;
+    MinNumNeigh = nan(size(dVal));
+    MaxNumNeigh = nan(size(dVal));
+    StdNumNeigh = nan(size(dVal));
+    for nIdx = 1:length(dVal)
+        MinNumNeigh(nIdx) = min(sum(z3_distanceMatrix<=dVal(nIdx),2)-1);
+        MaxNumNeigh(nIdx) = max(sum(z3_distanceMatrix<=dVal(nIdx),2)-1);
+        StdNumNeigh(nIdx) = std(sum(z3_distanceMatrix<=dVal(nIdx),2)-1);
+    end
+
+    nexttile(tldC)
+    plot(dVal,MinNumNeigh); hold on
+    plot(dVal,MaxNumNeigh)
+
+    xTick = logspace(log10(0.5+1),log10(3+1),6)-1;
+    set(gca,'XScale','log','XTick',xTick,'XLim',[dVal(1) dVal(end)])
+    sgtitle('effect of distance threshold on neighbor count')
+    ylabel('number of neighbor per channel')
+    xlabel('angular distance threshold [rad]')
+
+    yyaxis right
+    plot(dVal,StdNumNeigh)
+
+    xline(distance_z3_angular,'Color',0.5*[1 1 1],'LineStyle','--','LineWidth',0.25)
+    legend(["minimum" "maximum" "st. deviation" num2str(distance_z3_angular)])
+
+end
+
+
+
+
+%% neighborhood feedback (some descriptive stats) to user
+
+if pe_cfg.verbose
+    % min, max, median of neighbors per channel
+    disp(['on average, channels have a MEDIAN of      : '  num2str(median(neighborNum)) ' neighbors'])
+    disp(['the channel with fewest neighbors has (MIN): '  num2str(min(neighborNum)) ' neighbors'])
+    disp(['the channel with most neighbors has (MAX)  : '  num2str(max(neighborNum)) ' neighbors'])
+end
