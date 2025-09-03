eecbfd6a34eb5a22733d8ba5efdf5758e8bdf0cb
GermanoGallicchio <germano.gallicchio@gmail.com>
2025-09-03 01:27:50 +0100

    Enhance progress bar with ETA and optimize updates
    Added ETA display for large counts and switched to persistent variables for timing. Reduced the number of progress updates from 40 to 20 for less frequent output.

--- pe_counter.m
+++ pe_counter.m
@@ -1,14 +1,36 @@
 %% progress bar local function
-function pe_iterationCounter(counter,total)
+function pe_counter(counter,total)
+    persistent counting startTime
+
     if total > 1
+        % set persistent variables
         
+
         % display message
         if counter==1
             fprintf(['counting (of ' num2str(total) ' total): '])
         end
 
+        % start counting 
+        if counter==1  &&  total>=1000
+            counting = true;
+            startTime = tic;
+        end
+
+        % display ETA
+        if counter==100
+            if counting
+                elapsedTime = toc(startTime);
+                remainingTime = (elapsedTime*total/counter) - elapsedTime;
+                remainingMinutes = floor(remainingTime / 60);
+                remainingSeconds = mod(remainingTime, 60);
+                fprintf([ '      ' char(9203) ' ETA = ' num2str(remainingMinutes) 'min, ' num2str(round(remainingSeconds)) 'sec      '])
+                counting = false;
+            end
+        end
+
         % display the count
-        if any(counter==round(logspace(log10(1),log10(total),40)))
+        if any(counter==round(logspace(log10(1),log10(total),20)))
             fprintf([num2str(counter) ' '])
         end
 
